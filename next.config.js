16f8165b73489ccdf90eaaa1c118a013e952319d
Vano <vanovasilyan@gmail.com>
2022-10-22 18:11:28 +0400

    build generic Banner component and use it in Home component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true,
+  images: {
+    domains: ['bayut-production.s3.eu-central-1.amazonaws.com']
+  }
 }
 
 module.exports = nextConfig
